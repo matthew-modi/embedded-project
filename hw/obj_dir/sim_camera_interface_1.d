a8e7d20531f5f58b6f1c1151ad79bd5cf022e513
hab2175 <hab2175@micro21.ee.columbia.edu>
2025-05-13 18:40:36 -0400

    make clean

--- hw/obj_dir/sim_camera_interface_1.d
+++
@@ -1,5 +0,0 @@
-sim_camera_interface_1.o: ../sim_camera_interface_1.cpp \
- Vcamera_interface.h /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h \
- /usr/share/verilator/include/verilated_vcd_c.h \
- /usr/share/verilator/include/verilated.h
