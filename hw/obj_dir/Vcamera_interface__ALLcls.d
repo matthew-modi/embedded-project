ec9ef105cc05f767cd690f61c09e777dbed1675d
Workstation <fake.email@example.com>
2025-05-14 17:20:48 -0400

    cleaned

--- hw/obj_dir/Vcamera_interface__ALLcls.d
+++
@@ -1,4 +0,0 @@
-Vcamera_interface__ALLcls.o: Vcamera_interface__ALLcls.cpp \
- Vcamera_interface.cpp Vcamera_interface.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h Vcamera_interface__Syms.h
--- hw/soc_system/synthesis/submodules/my_dff_en.sv
+++
@@ -1,7 +0,0 @@
-// D-flip-flop with clock enable signal for debouncing module 
-module my_dff_en(input DFF_CLOCK, clock_enable,D, output reg Q=0);
-    always @ (posedge DFF_CLOCK) begin
-  if(clock_enable==1) 
-           Q <= D;
-    end
-endmodule 
