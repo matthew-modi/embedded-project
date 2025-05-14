a46cd78c4b0329ee4bf3fa184b72a11573bc853d
Matthew Modi <22771068+matthew-modi@users.noreply.github.com>
2025-05-14 01:34:44 -0400

    clarify signals

--- hw/vga_interface.sv
+++ hw/vga_interface.sv
@@ -4,11 +4,11 @@
 
 module vga_interface (
   input logic       clk,         // 25 MHz PLL
+  input  logic        reset,       // active‐high synchronous reset
 
   output logic      xclk,       // 25 MHz pixel clock to camera
 
-  input  logic        pclk,        // 25 MHz pixel clock from camera
-  input  logic        reset,       // active‐high synchronous reset
+  input  logic        cam_pclk,        // 25 MHz pixel clock from camera
   input  logic        cam_href,    // row‐valid from camera
   input  logic        cam_vsync,   // frame‐valid from camera
   input  logic [7:0]  cam_data,    // one byte of RGB565 per cycle
